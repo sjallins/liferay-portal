509ed1bdf5984698166879c76cb6db75543b74d8
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2013-07-19 10:33:08 +0200

    LPS-37929 Finished adapting RSSAction

--- portal-impl/src/com/liferay/portlet/assetpublisher/action/RSSAction.java
+++ portal-impl/src/com/liferay/portlet/assetpublisher/action/RSSAction.java
@@ -14,83 +14,43 @@
 
 package com.liferay.portlet.assetpublisher.action;
 
-import com.liferay.portal.kernel.language.LanguageUtil;
-import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
-import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.Http;
-import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.model.Layout;
-import com.liferay.portal.theme.PortletDisplay;
-import com.liferay.portal.theme.ThemeDisplay;
-import com.liferay.portal.util.Portal;
-import com.liferay.portal.util.PortalUtil;
-import com.liferay.portal.util.WebKeys;
-import com.liferay.portlet.asset.AssetRendererFactoryRegistryUtil;
-import com.liferay.portlet.asset.model.AssetEntry;
-import com.liferay.portlet.asset.model.AssetRenderer;
-import com.liferay.portlet.asset.model.AssetRendererFactory;
-import com.liferay.portlet.assetpublisher.util.AssetPublisherUtil;
-import com.liferay.util.RSSUtil;
-
-import com.sun.syndication.feed.synd.SyndContent;
-import com.sun.syndication.feed.synd.SyndContentImpl;
-import com.sun.syndication.feed.synd.SyndEntry;
-import com.sun.syndication.feed.synd.SyndEntryImpl;
-import com.sun.syndication.feed.synd.SyndFeed;
-import com.sun.syndication.feed.synd.SyndFeedImpl;
-import com.sun.syndication.feed.synd.SyndLink;
-import com.sun.syndication.feed.synd.SyndLinkImpl;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import javax.portlet.PortletPreferences;
-import javax.portlet.PortletRequest;
-import javax.portlet.PortletResponse;
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
+
+import com.liferay.portlet.rss.RSSRenderer;
+import com.liferay.portlet.rss.action.DefaultRSSAction;
 
 /**
  * @author Brian Wing Shun Chan
  * @author Julio Camarero
  */
-public class RSSAction extends com.liferay.portal.struts.RSSAction {
+public class RSSAction extends DefaultRSSAction {
+
 
 	@Override
 	protected byte[] getRSS(
-			ResourceRequest portletRequest, ResourceResponse portletResponse)
+		ResourceRequest portletRequest, ResourceResponse portletResponse)
 		throws Exception {
-
+		
 		PortletPreferences portletPreferences = portletRequest.getPreferences();
-
+		
 		String selectionStyle = portletPreferences.getValue(
 			"selectionStyle", "dynamic");
-
+	
 		if (!selectionStyle.equals("dynamic")) {
 			return new byte[0];
 		}
-
-		String assetLinkBehavior = portletPreferences.getValue(
-			"assetLinkBehavior", "showFullContent");
-		String rssDisplayStyle = portletPreferences.getValue(
-			"rssDisplayStyle", RSSUtil.DISPLAY_STYLE_ABSTRACT);
-		String rssFeedType = portletPreferences.getValue(
-			"rssFeedType", RSSUtil.FEED_TYPE_DEFAULT);
-		String rssName = portletPreferences.getValue("rssName", null);
-
-		String format = RSSUtil.getFeedTypeFormat(rssFeedType);
-		double version = RSSUtil.getFeedTypeVersion(rssFeedType);
-
-		String rss = exportToRSS(
-			portletRequest, portletResponse, rssName, null, format, version,
-			rssDisplayStyle, assetLinkBehavior,
-			getAssetEntries(portletRequest, portletPreferences));
-
-		return rss.getBytes(StringPool.UTF8);
+		
+		return super.getRSS(portletRequest, portletResponse);
+	}
+	
+	@Override
+	protected RSSRenderer createRSSRenderer(
+		ResourceRequest portletRequest, ResourceResponse portletResponse)
+		throws Exception {
+		
+		return new AssetRSSRenderer(portletRequest, portletResponse);
 	}
 
 }
